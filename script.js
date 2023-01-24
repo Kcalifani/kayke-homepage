d4ca0c2052ac178ae277d09e7a73c56d23a8c3af
Kcalifani <kaykegamer1703@gmail.com>
2023-01-24 17:22:55 -0200

    Inserindo codigo JS para rodar botão.

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleModde() {
+  const html = document.documentElement
+
+
+  if(html.classList.contains('light')) {
+    html.classList.remove('light') 
+  } else {
+    html.classList.add('light')
+  }
+}
