50f6bb700bc5bf2d139ded59a08b6902a7f10fe7
Kcalifani <kaykegamer1703@gmail.com>
2023-01-24 18:03:33 -0200

    Colocando segunda imagem.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function toggleModde() {
   const html = document.documentElement
-
-
-  /*if(html.classList.contains('light')) {
-    html.classList.remove('light') 
-  } else {
-    html.classList.add('light')
-  }*/
   html.classList.toggle('light')
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if(html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
